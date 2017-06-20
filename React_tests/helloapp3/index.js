24e194b4e2c7b2092c38c302c2c75c13abf77278
loktevalexey <loktev.alex.74@gmail.com>
2017-06-20 11:09:21 +0300

    moved to git from local folders

---
+++ React_tests/helloapp3/index.js
@@ -0,0 +1,6 @@
+﻿ReactDOM.render(
+<h1 id='title'>
+Hello React 3
+</h1>, // элемент, который мы хотим создать
+document.getElementById("container1") // где мы этот элемент хотим создать
+)
---
+++ React_tests/helloapp3/target.js
@@ -0,0 +1,9 @@
+"use strict";
+
+ReactDOM.render(React.createElement(
+  "h1",
+  { id: "title" },
+  "Hello React 3"
+), // элемент, который мы хотим создать
+document.getElementById("container1") // где мы этот элемент хотим создать
+);
